3e7512312b04177def37f7bc1bf997a3088bdc18
mw08081 <president304@naver.com>
2022-12-13 16:25:19 +0900

    New idea for 2675.cpp

--- backJoonCpp/2675.cpp
+++ backJoonCpp/2675.cpp
@@ -11,30 +11,10 @@ int main()
     for (int i = 0; i < t; i++)
     {
         cin >> time >> s;
-        char * arr = new char[s.length() * time];
 
         for (int j = 0; j < s.length() * time; j++)
-        {
-            arr[j] = s[j / time];
-            cout << arr[j];
-        }
+            cout << s[j / time];
+
         cout << '\n';
     }
-    
-    /*
-    int t, time;
-    string s;
-
-    cin >> t;
-    for (int i = 0; i < t; i++)
-    {
-        cin >> time >> s;
-        for (int j = 0; j < s.length(); j++)
-        {
-            for (int k = 0; k < time; k++)
-                cout << s[j];
-            cout << '\n';
-        }
-    }
-    */
 }
