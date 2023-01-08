da7fae4b71ba2cd77616951ccc12216f2f657108
mw08081 <president304@naver.com>
2023-01-08 20:08:10 +0900

    Added 2869, Created 10250

--- backJoonCpp/2869.cpp
+++ backJoonCpp/2869.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -6,5 +7,5 @@ int main()
     int a, b, v;
     cin >> a >> b >> v;
 
-    
+    cout << (int)(ceil((double)(v - a) / (a - b)) + 1);
 }
