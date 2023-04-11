7308fcf3420b3441e84fd1fdb18166d33251f437
mw08081 <president304@naver.com>
2023-04-11 14:02:00 +0900

    Added 2745.cpp

---
+++ backJoonCpp/2745.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int res = 0, B;
+    std::string N;
+    std::cin >> N >> B;
+
+    for(auto e : N)
+        res = res*B + ((e >= 65 ? e-55 : e-48));
+    std::cout << res;
+}
