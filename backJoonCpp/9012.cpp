4960935976b5bf61d4480db6ddad531d0575586f
mw08081 <president304@naver.com>
2023-05-03 09:28:22 +0900

    Added 9012

---
+++ backJoonCpp/9012.cpp
@@ -0,0 +1,31 @@
+#include <stack>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    string s;
+    int t;
+    cin >> t;
+
+    while(t--){
+        cin >> s;
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(') st.push('(');
+            else 
+            {
+                if(st.empty()) {
+                    st.push(')');
+                    break;
+                }
+                st.pop();
+            }
+        }
+        
+        cout << (st.empty() ? "YES" : "NO");
+    }
+}
