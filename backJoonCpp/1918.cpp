e5293d53e2fb369c74b5b3a280282178db24480b
mw08081 <president304@naver.com>
2023-12-01 14:46:37 +0900

    Added 1918.cpp

---
+++ backJoonCpp/1918.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    stack<char> s;
+
+    cin >> str;
+    for(auto e : str) {
+        if( e >= 65 && e <= 90) cout << e;
+        else {
+            if (s.empty()) {
+                s.push(e);
+            }
+            else if(e == '(') s.push(e);
+            else if(e == ')') {
+                while(s.top() == '(') {
+                    if(s.top() != '(') cout << s.top();
+                    s.pop();
+                }
+            }
+            else if(e == '*' || e == '/') {
+                while(s.top() != '(' && (s.top() == '*' || s.top() == '/')) {
+                    if(s.top() != '(') cout << s.top();
+                    s.pop();
+                }
+                s.push(e);
+            }
+            else {
+                while(!s.empty() && s.top() != '(') {
+                    if(s.top() != '(') cout << s.top();
+                    s.pop();
+                }
+                s.push(e);
+            }
+        }
+    }
+    
+    while(!s.empty()) {
+        if(s.top() != '(') cout << s.top();
+        s.pop();
+    }
+}
