f875118c2f077c223a499a49c093a8ceb1cfd168
mw08081 <president304@naver.com>
2024-04-17 15:01:11 +0900

    ADDED 1935

--- backJoonCpp/1935.cpp
+++ backJoonCpp/1935.cpp
@@ -3,38 +3,40 @@
 
 using namespace std;
 
-float Calc(float n1, float n2, char op)
+double Calc(double n1, double n2, char op)
 {
     if(op == '+') return n1 + n2;
     else if(op == '-') return n1 - n2;
     else if(op == '*') return n1 * n2;
-    else if(op == '/') return n1 / n2;
-    else return 0;
+    else return n1 / n2;
 }
 
 int main()
 {
-    bool isIn = false;
-    int n, res = 0, v[25] = {0};
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    double n, res = 0, v[25] = {0}, n1, n2;
     string f;
-    stack<char> s;
+    stack<double> s;
 
     cin >> n >> f;
     for (int i = 0; i < n; i++)
         cin >> v[i];
 
     for(auto c : f){
-        if(c >= 65 && c <= 90) s.push(c);
+        if(c >= 65 && c <= 90) s.push(v[c-65]);
         else{
-            if(!isIn) {
-                res = s.top();
-                s.pop();
-                res = Calc(res, s.top(), c);
-                s.pop();
-            }
-            else {
-                
-            }
+            n2 = s.top();
+            s.pop();
+            n1 = s.top();
+            s.pop();
+
+            s.push(Calc(n1,n2,c));
         }
     }
+    cout<<fixed;
+    cout.precision(2);
+    cout<<s.top();
 }
