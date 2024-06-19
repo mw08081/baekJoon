f6d9cc40b0306b2f483d1bd8be45422227e1877e
mw08081 <president304@naver.com>
2024-06-19 17:14:36 +0900

    Adding 6588.cpp

---
+++ backJoonCpp/6588.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX 1000001
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    vector<int> isPrime(MAX);
+    int n;
+
+    for(int i = 2; i <= MAX; i++)
+        for(int j = 1; j*i < MAX; j++)
+            if(isPrime[j*i] == 0)
+                isPrime[i*j] = 1;
+
+            
+    for (int i = 2 ; i < 100; i++)
+        if(isPrime[i] == 0)
+            cout << i << endl;
+    return 0;
+    cin >> n;
+    while(n--){
+
+    }
+}
