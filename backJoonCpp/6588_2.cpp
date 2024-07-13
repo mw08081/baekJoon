31a79be05f45d75fbbd6c8d371d182a7f686aeb7
mw08081 <president304@naver.com>
2024-07-13 18:38:42 +0900

    Added 6588_2.cpp

--- backJoonCpp/6588_2.cpp
+++ backJoonCpp/6588_2.cpp
@@ -9,7 +9,6 @@ int main()
     cout.tie(NULL);
 
     vector<int> isPrime(MAX);
-    int n=-1;
 
     isPrime[0] = isPrime[1] = 1;
     for(int i = 2; i*i <= MAX; i++)
@@ -17,16 +16,17 @@ int main()
             for(int j = 2; j*i < MAX; j++)
                 isPrime[i*j] = 1; 
 
-    while(n){
-        cin >> n;
-        if(n==0)break;
-
+    for(int j = 6; j < MAX; j+=2)
+    {
         bool hasAns = false;
-        for(int i = 3; i < n; i+=2)
+        for(int i = 3; i <= MAX; i+=2)
         {
-            if(!isPrime[i] && !isPrime[n-i])
+            if(!isPrime[i] && !isPrime[j-i])
             {
-                cout << n << " = " << i << " + " << n - i << '\n';
+                if(i > MAX/2)
+                    cout << j << " = " << i << " + " << j - i << '\n';
+                //else    
+                    //cout << "---;
                 hasAns = true;
                 break;
             }
