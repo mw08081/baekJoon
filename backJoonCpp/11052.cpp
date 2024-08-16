653eb5c922e82b0ae80bec480f03a93d85d4a882
mw08081 <president304@naver.com>
2024-08-16 11:56:33 +0900

    added  11052, 16194

---
+++ backJoonCpp/11052.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define MAX 1001
+
+using namespace std;
+
+int dp[MAX];
+int p[MAX];
+
+void Get(int v)
+{
+    vector<int> vs;
+
+    for (int i = 1; i < v; i++)
+        vs.push_back(dp[i] + dp[v-i]);
+    vs.push_back(p[v]);
+
+    dp[v] = *max_element(vs.begin(), vs.end());
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 1; i < N+1; i++)
+        cin >> p[i];
+
+    for (int i = 1; i < N+1; i++)
+        Get(i);
+    
+    cout << dp[N];
+}
