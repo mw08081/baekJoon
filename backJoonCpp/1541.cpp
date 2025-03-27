69037e5483c805d8eb154cc4f02d423c3495684e
mw08081 <president304@naver.com>
2025-03-27 17:02:39 +0900

    ADDED 1541.cpp

--- backJoonCpp/1541.cpp
+++ backJoonCpp/1541.cpp
@@ -1,18 +1,53 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 using namespace std;
 
-int main()
-{
-    vector<string> c;
-    string f;
-    cin >> f;
+string n = "";
+stack<int> ns;
+stack<char> os;
+bool b = false;
 
-    for (int i = 0; i < f.length(); i++)
-    {
-        if(f[i] == '=') {
+void f() {
+    int t = stoi(n);
+    if(b) {
+        t += ns.top();
+        ns.pop();
+        b = false;
+    }
+    ns.push(t);
+    n = "";
+}
+
+int main() 
+{
+    int res = 0;
+    string exp;
+    cin >> exp;
+
+    for(auto e : exp){
+        if(e == '+'|| e == '-'){
+            // 숫자 처리
+            f();
             
+            // 연산자 계산
+
+            if(!os.empty() && os.top() == '-' && e == '+'){
+                b = true;
+            } else {
+                os.push(e);
+            }
+        } else {
+            n += e;
         }
     }
-    
+    f();
+
+    while(!ns.empty()){
+        if(!os.empty() && os.top() == '-') res -= ns.top();
+        else res += ns.top();
+        
+        ns.pop();
+        if(!os.empty()) os.pop();
+    }
+    cout << res;
 }
