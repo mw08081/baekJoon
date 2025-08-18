cf5f048c2d4efdc0b43ca51566665ac80b182cd9
Octa <58582985+mw08081@users.noreply.github.com>
2025-08-18 09:47:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.24 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    std::unordered_map<std::string, int> map;
+    
+    for(auto c : completion)
+    {
+        auto it = map.find(c);
+        if(it != map.end()) it->second += 1;
+        else map[c] = 1;
+    }
+    
+    for(auto p : participant)
+    {
+        auto it = map.find(p);
+        if(it != map.end()){
+            if(it->second > 0) it->second -= 1;
+            else return p;
+        }
+        else return p;
+    } 
+}
