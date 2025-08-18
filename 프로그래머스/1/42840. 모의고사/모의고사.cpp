d054ba1c1604f6c06c3106df600f48439c4f4e49
Octa <58582985+mw08081@users.noreply.github.com>
2025-08-18 11:14:09 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    int a[] = {1, 2, 3, 4, 5};
+    int b[] = {2, 1, 2, 3, 2, 4, 2, 5};
+    int c[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    int ac = 0, bc = 0, cc = 0;
+    int ai = 5, bi = 8, ci = 10;
+    
+    for(int i = 0;i < answers.size(); i++){
+        if(answers[i] == a[i%ai]) ac++;
+    }
+    for(int i = 0;i < answers.size(); i++){
+        if(answers[i] == b[i%bi]) bc++;
+    }
+    for(int i = 0;i < answers.size(); i++){
+        if(answers[i] == c[i%ci]) cc++;
+    }
+    
+    int mv = max({ac,bc,cc});
+    if(ac == mv) answer.push_back(1);
+    if(bc == mv) answer.push_back(2);
+    if(cc == mv) answer.push_back(3);
+    
+    return answer;
+}
