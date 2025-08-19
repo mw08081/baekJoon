65585cba825f9a2813415e6415b6344797329043
Octa <58582985+mw08081@users.noreply.github.com>
2025-08-19 11:42:03 +0900

    [level 2] Title: 전화번호 목록, Time: 1538.95 ms, Memory: 231 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <iostream>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    unordered_set<string> s;
+    
+    // n log n
+    sort(phone_book.begin(), phone_book.end(), [](const string& a, const string& b){
+       return a.size() > b.size();
+    });
+    
+    for(auto e : phone_book){
+        if(s.insert(e).second == false) return false;
+        
+        for(int i = 1; i < e.size(); i++) 
+            s.insert(e.substr(0,i));
+    }
+    
+    return true;
+}
