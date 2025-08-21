3a05df8ee9cd944ffee954ca2388f0273be19ead
Octa <58582985+mw08081@users.noreply.github.com>
2025-08-21 09:44:13 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.29 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -5,5 +5,18 @@
 using namespace std;
 
 vector<int> solution(int n, int m) {
-    return {gcd(n,m), lcm(n, m)};
+    int t, mv = n*m;
+    if(n < m) {
+        t = m;
+        m = n;
+        n = t;
+    }
+    
+    while(m > 0){
+        t = m;
+        m = n%m;
+        n = t;
+    }
+    return {n, mv/n};
+    // return {gcd(n,m), lcm(n, m)};
 }
