752630bf376bcbe8811328d06d624b23813cb3f9
Octa <58582985+mw08081@users.noreply.github.com>
2025-08-26 09:43:18 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.46 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -1,13 +1,11 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <list>
 
 using namespace std;
 
 string solution(vector<int> food) {
     list<char> lst = {'0'};
-    string answer = "";
     for(int i = food.size() - 1; i > 0; i--) {
         for(auto c = 0; c < food[i] / 2; c++) {
             lst.push_back(i + '0');
