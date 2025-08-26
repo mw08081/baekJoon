815c6fee19e02bfdeef10a9cba82f5652bdab0f4
Octa <58582985+mw08081@users.noreply.github.com>
2025-08-26 10:02:00 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    
+    while(n >= a) {
+        answer += (n / a) * b;
+        n = (n / a) * b + (n % a);
+    }
+    return answer;
+}
