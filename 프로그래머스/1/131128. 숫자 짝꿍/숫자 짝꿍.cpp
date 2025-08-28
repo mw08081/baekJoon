f8b4d32575cef8105fcf9d97c69a2a784ec3d823
Octa <58582985+mw08081@users.noreply.github.com>
2025-08-28 12:27:20 +0900

    [level 1] Title: 숫자 짝꿍, Time: 188.08 ms, Memory: 29.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string X, string Y) {
+    string answer = "";
+    vector<char> t;
+    int xArr[10] = {0};
+    int yArr[10] = {0};
+    
+    for(auto e : X) xArr[e - '0']++;
+    for(auto e : Y) yArr[e - '0']++;
+    
+    for(auto i = 9; i >= 0; i--)
+        if(xArr[i] > 0 && yArr[i] > 0)
+            for(auto j = 0; j < min(xArr[i], yArr[i]); j++)
+                answer += to_string(i);
+    
+    if(answer.empty()) return "-1";
+    else if(answer[0] == '0') return "0";
+    return answer;
+}
