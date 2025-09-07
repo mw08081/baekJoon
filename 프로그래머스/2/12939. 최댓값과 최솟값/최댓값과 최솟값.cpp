434cd997e103aa8e1eb58ef098ce1e1a478fadf6
Octa <58582985+mw08081@users.noreply.github.com>
2025-09-07 23:42:33 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 3.78 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s) {
+    int stdIdx = 0;
+    vector<long long> vec;
+    
+    for(auto i = 0; i < s.size(); i++) {
+        if(s[i] == ' '){
+            auto v = s.substr(stdIdx, i-stdIdx+1);
+            auto numV = stoll(v);
+            
+            vec.push_back(numV);
+            stdIdx = i+1;
+        }
+    }
+    vec.push_back(stoll(s.substr(stdIdx, s.size()-1)));
+
+    
+    auto res = minmax_element(vec.begin(),vec.end());
+    return to_string(*res.first) + " " + to_string(*res.second);
+}
