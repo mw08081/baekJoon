91e49ce4882bd5429145ddb1bb1b2c0fa742ed02
Octa <58582985+mw08081@users.noreply.github.com>
2025-09-08 09:19:24 +0900

    [level 2] Title: 피보나치 수, Time: 3.12 ms, Memory: 6.98 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -6,13 +6,10 @@ using namespace std;
 int dp[100001] = {0,1};
 
 int solution(int n) {
-    if(n == 0) return 0;
-    if(n == 1) return 1;
+    if(n <= 1) return n;
     
-    if(dp[n] == 0) {
+    if(dp[n] == 0) 
         dp[n] = (solution(n-1) + solution(n-2)) % 1234567;
-        return dp[n];
-    } else {
-        return dp[n];
-    }
+    
+    return dp[n];
 }
