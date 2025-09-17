079917ea27e29201f388be503150b997dd7f3c48
mw08081 <president304@naver.com>
2025-09-17 17:32:38 +0900

    ADDing 1715

--- backJoonCpp/1715.cpp
+++ backJoonCpp/1715.cpp
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <algorithm>
+#include <list>
 using namespace std;
 
 int main()
 {
     long long c, t=0;
-    int n;
-    int a[100000] = {0};
+    int n,v ;
+    list<int> l;
 
     scanf("%d", &n);
-    for(int i = 0; i < n; i++) scanf("%d", &a[i]);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &v);
+        l.push_back(v);
+    }   
+    l.sort();
+    for(auto x : l) printf("%d ", x);
 
-    sort(a, a+n);
     
-    c = a[0];
-    for(int i = 1; i < n; i++){
-        c += a[i];
-        t += c;
-    }
+
     printf("%lld", t);
 }
